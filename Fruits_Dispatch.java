6e83b4a4891b260b684515824f522da2c2f06f6a
MahammadFaqruddin <142114843+MahammadFaqruddin@users.noreply.github.com>
2023-08-30 01:30:58 +0530

    Add files via upload
    Java Program by using Abstract Dispatch by taking Fruits  as a class

---
+++ Fruits_Dispatch.java
@@ -0,0 +1,29 @@
+class Apple{
+    void display(){
+        System.out.println("Inside Apple's display method");
+
+    }
+}
+class Banana extends Apple{
+    void display(){
+        System.out.println("Inside Banana's display method");
+
+    }
+}
+class Cherry extends Apple{
+    void display() { 
+        System.out.println("Inside cherry's display method");
+}
+}
+class Fruits_Dispatch{
+    public static void main(String args[]){
+        Apple a=new Apple();
+        Banana b=new Banana();
+        Cherry c=new Cherry();
+        Apple ref;
+        ref=a;
+        ref.display();
+        ref=b;
+        ref.display();
+    }
+}
